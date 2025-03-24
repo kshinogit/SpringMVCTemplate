12851a3330e3fdecc76ab8e79dbac63c46400bb9
kazuya shinohara <kazuyas0323@gmail.com>
2025-03-24 17:01:36 +0900

    ログ出力機能追加

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.controller;
 
 import com.example.config.AppProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class HelloController {
 
     @Autowired
@@ -14,6 +16,8 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(Model model) {
+        System.out.println("Current Working Directory: " + System.getProperty("user.dir"));
+        log.info("[start] HelloController");
         model.addAttribute("message", "Thymeleafを使ったSpring MVCプロジェクト");
         model.addAttribute("appName", appProperties.getAppName());
         model.addAttribute("appVersion", appProperties.getAppVersion());
